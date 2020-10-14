241d18091cf0aa7cd533c242f0488b9982f74a16
Blake <mammenblake019@gmail.com>
2020-10-14 17:10:49 -0500

    Replace human file size function

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -9,7 +9,8 @@ function episode_tag(season, episode) {
 
 function number_format(num) {
 	var num = parseInt(num);
-	if(num<1000) return num;
+	if(num<1000) 
+		return num;
 	var num = num.toString();
 	var rgx = /(\d+)(\d{3})/;
 	while (rgx.test(num)) {
@@ -19,24 +20,31 @@ function number_format(num) {
 } // end func: number_format
 
 function inflect(num, single, plural) {
-	if(num==1) return single;
-	if(plural) return plural;
+	if(num==1) 
+		return single;
+	if(plural) 
+		return plural;
 	return single+"s";
 } // end func: inflect
 
-function hl_bytes_to_human(bytes) {
-	var b = parseInt(bytes);
-	if(b < 1024) return b+' b';
-	var kb = b/1024;
-	if(kb < 1024) return Math.round(kb*100)/100+' Kb';
-	var mb = kb/1024;
-	if(mb < 1024) return Math.round(mb*100)/100+' Mb';
-	var gb = mb/1024;
-	if(gb < 1024) return Math.round(gb*100)/100+' Gb';
-	var pb = gb/1024;
-	return Math.round(pb*100)/100+' Pb';
-	if(bytes < 1073741824) return Math.round(bytes/1048576*100)/100+' Mb';
-	var kb = b/1024;
-	if(bytes < 1099511627776) return Math.round(bytes/1073741824*100)/100+' Gb';
-	return bytes+' Xb';
-} // end func: hl_bytes_to_human
+function humanFileSize(bytes, si=false, dp=1) {
+	const thresh = si ? 1000 : 1024;
+  
+	if (Math.abs(bytes) < thresh) {
+	  return bytes + ' B';
+	}
+  
+	const units = si 
+	  ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'] 
+	  : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+	let u = -1;
+	const r = 10**dp;
+  
+	do {
+	  bytes /= thresh;
+	  ++u;
+	} while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+  
+  
+	return bytes.toFixed(dp) + ' ' + units[u];
+  } // end func: humanFileSize
